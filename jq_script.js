42e51b2f7c145a34867f42273c8b75651af3e870
Andrey Komov <andrewkomov@gmail.com>
2022-05-16 20:36:22 +0300

    some progress

--- jq_script.js
+++ jq_script.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    $('.block').slideDown(5000, ()=> console.log('Alright, alright, alright...'));
+
     $('#get').on('click', function () {
         $.get('http://test.local/request.php?name=andrew&age=34', function (response){
             console.log(response);
@@ -23,15 +25,30 @@ $(function() {
             table+=`<td>${obj.name}</td> 
                 <td>${obj.model}</td>
                 <td>${obj.year}</td>
-                <td>${obj.color}</td>`;
+                <td>${obj.color}</td>
+                <td><button class="carData" data-model="${obj.model}" id="${obj.name}">Car Data</button></td>`;
             table += '</tr>';
         });
+
         table+='</table>';
 
         $('.mainContent').html(table);
+
+        $('.carData').click ( function (event) {
+            event.preventDefault();
+            alert(event.target.id);
+            }
+        );
+
     }
 
 })
+
+
+
+
+
+
 
 
 
